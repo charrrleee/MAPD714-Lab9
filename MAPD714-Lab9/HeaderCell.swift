2bc0b916585b1ab118fdbd7ed38bee299b294a86
charrrleee <charlcpy@gmail.com>
2022-11-09 23:38:22 -0500

    complete dialog view

---
+++ MAPD714-Lab9/HeaderCell.swift
@@ -0,0 +1,27 @@
+//
+//  HeaderCell.swift
+//  MAPD714-Lab9
+//
+//  Created by Charlene Cheung on 9/11/2022.
+//
+
+import UIKit
+
+class HeaderCell: ContentCell {
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        label.backgroundColor = UIColor(red: 0.9, green: 0.9,
+                                        blue: 0.8, alpha: 1.0)
+        label.textColor = UIColor.black
+    }
+    
+    required init?(coder aDecoder: NSCoder)
+    {
+        super.init(coder: aDecoder)
+    }
+    
+    override class func defaultFont() -> UIFont
+    {
+        return UIFont.preferredFont(forTextStyle: UIFont.TextStyle.headline)
+    }
+}
