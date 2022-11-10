e374d5b2cbe793967ecc5f0518689dc3f32d877a
charrrleee <charlcpy@gmail.com>
2022-11-09 22:31:22 -0500

    Initial Commit

---
+++ MAPD714-Lab9/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MAPD714-Lab9
+//
+//  Created by Charlene Cheung on 9/11/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ MAPD714-Lab9UITests/MAPD714_Lab9UITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  MAPD714_Lab9UITestsLaunchTests.swift
+//  MAPD714-Lab9UITests
+//
+//  Created by Charlene Cheung on 9/11/2022.
+//
+
+import XCTest
+
+final class MAPD714_Lab9UITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
